15493e25779757b216a20ea91692d1a22036ccad
luisanuneze <luisanuneze@gmail.com>
2024-09-27 20:25:27 -0600

    Products resource

---
+++ database/migrations/2024_09_28_022024_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('category_id');
+            $table->string('title');
+            $table->string('description');
+            $table->string('sku')->nullable();
+            $table->string('currency');
+            $table->decimal('price');
+            $table->decimal('order_price');
+            $table->string('stock');
+            $table->boolean('available');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
