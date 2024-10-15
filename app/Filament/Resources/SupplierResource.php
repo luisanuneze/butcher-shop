2ed144c41b7baa591837c0ef3028876d9013f679
Daniel Rodriguez <danielrafaelrr@hotmail.com>
2024-10-15 16:51:30 -0600

    Crud suppliers Done :)

--- app/Filament/Resources/SupplierResource.php
+++ app/Filament/Resources/SupplierResource.php
@@ -23,16 +23,34 @@ class SupplierResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('name')
-                ->required(),
-                Forms\Components\TextInput::make('contact_name'),
-                Forms\Components\TextInput::make('address')
-                ->required(),
-                Forms\Components\TextInput::make('phone')
-                ->required(),
-                Forms\Components\TextInput::make('email')
-                ->required(),
-                Forms\Components\TextInput::make('website'),
+            Forms\Components\TextInput::make('name')
+                ->required()
+                ->label('Nombre'),
+            Forms\Components\TextInput::make('contact_name')
+                ->label('Nombre de Contacto'),
+            Forms\Components\TextInput::make('address')
+                ->required()
+                ->label('Dirección'),
+            Forms\Components\TextInput::make('phone')
+                ->required()
+                ->tel()
+                ->label('Teléfono'),
+            Forms\Components\TextInput::make('email')
+                ->required()
+                ->email()
+                ->label('Correo Electrónico'),
+            Forms\Components\TextInput::make('website')
+                ->label('Sitio Web')
+                ->url()
+                ->reactive()
+                ->afterStateUpdated(function ($state, $set, $get) {
+                    if ($state && !preg_match('/^https?:\/\//', $state)) {
+                        $set('website', 'https://' . $state);
+                    }
+                }),
+            Forms\Components\TextInput::make('city')
+                ->required()
+                ->label('Ciudad'),
             ]);
     }
 
@@ -40,7 +58,43 @@ class SupplierResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Nombre')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('contact_name')
+                    ->label('Nombre de Contacto')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('address')
+                    ->label('Dirección')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('phone')
+                    ->label('Teléfono')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->label('Correo Electrónico')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('website')
+                    ->label('Sitio Web')
+                    ->url(fn ($record) => $record->website)
+                    ->openUrlInNewTab()
+                    ->formatStateUsing(function ($state) {
+                        return $state ? parse_url($state, PHP_URL_HOST) : 'N/A';
+                    })
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('city')
+                    ->label('Ciudad')
+                    ->searchable()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->label('Fecha de Creación')
+                    ->dateTime('d M Y')
+                    ->sortable(),
             ])
             ->filters([
                 //
@@ -71,3 +125,4 @@ class SupplierResource extends Resource
         ];
     }
 }
+
